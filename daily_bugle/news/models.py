43844dfc266ab3e60a262c857ba3d123cdbbbe66
nabilfadjar <nabilfadjar@gmail.com>
2017-11-16 18:27:10 +0000

    Added New Models

--- daily_bugle/news/models.py
+++ daily_bugle/news/models.py
@@ -4,31 +4,42 @@ from django.db import models
 class User(model.Models):
 	full_name = models.CharField(length=255)
 	email = models.EmailField(length=254, unique=True)
-	password = models.CharField(length=255)
+	password = models.CharField(length=255) # We need to store Hashed Password - Figure out later
 	phone_number = models.CharField(length=20)
-    def __str__(self):
-        return self.question_text
+
+	def __str__(self):
+        return self.email
 
 class Article(model.Models):
 	title = models.CharField(length=255)
 	text = models.TextField()
 	pub_date = models.DateTimeField('date published')
-	author_id = # User ID - Many -> One / Many Articles -> one author
-	comment_id = # many -> many many / many comments posted to many articles
-    def __str__(self):
-        return self.question_text
+	author_id = models.ForeignKey(User, on_delete=models.CASCADE) # User ID - Many -> One / Many Articles -> One author
+	category_id = models.ForeignKey(Category, on_delete=models.CASCADE) # Category ID - One -> One / One Articles -> One Category
+
+	def __str__(self):
+        return self.pk
 
 class Category(model.Models):
-	name =
-    def __str__(self):
-        return self.question_text
+	name = models.CharField(length=255)
+
+	def __str__(self):
+        return self.name
 
 class Comment(model.Models):
     text = models.TextField()
+	author_id = models.ForeignKey(User) # User ID - Many -> One / Many Articles -> one author
     pub_date = models.DateTimeField('date published')
-
-    def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+	article_id = models.ForeignKey(Article, on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.question_text
+        return self.pk
+
+class Like(model.Models):
+    isLike = models.NullBooleanField()
+	author_id = models.ForeignKey(User) # User ID - Many -> One / Many Articles -> one author
+    pub_date = models.DateTimeField('date published')
+	article_id = models.ForeignKey(Article, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.pk
