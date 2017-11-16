e45faa9592e2a7869aee9dab914c2c85cd219c5a
nabilfadjar <nabilfadjar@gmail.com>
2017-11-16 18:46:24 +0000

    Refractored Tabs to Spaces :(

--- daily_bugle/news/models.py
+++ daily_bugle/news/models.py
@@ -1,45 +1,45 @@
 from django.db import models
 
 # Create your models here.
-class User(model.Models):
-	full_name = models.CharField(length=255)
-	email = models.EmailField(length=254, unique=True)
-	password = models.CharField(length=255) # We need to store Hashed Password - Figure out later
-	phone_number = models.CharField(length=20)
+class User(models.Model):
+    full_name = models.CharField(max_length=255)
+    email = models.EmailField(max_length=254, unique=True)
+    password = models.CharField(max_length=255) # We need to store Hashed Password - Figure out later
+    phone_number = models.CharField(max_length=20)
 
-	def __str__(self):
+    def __str__(self):
         return self.email
-		
-class Article(model.Models):
-	title = models.CharField(length=255)
-	text = models.TextField()
-	pub_date = models.DateTimeField('date published')
-	author_id = models.ForeignKey(User, on_delete=models.CASCADE) # User ID - Many -> One / Many Articles -> One author
-	category_id = models.ForeignKey(Category, on_delete=models.CASCADE) # Category ID - One -> One / One Articles -> One Category
 
-	def __str__(self):
-        return self.pk
-
-class Category(model.Models):
-	name = models.CharField(length=255)
-
-	def __str__(self):
-        return self.name
-
-class Comment(model.Models):
-    text = models.TextField()
-	author_id = models.ForeignKey(User) # User ID - Many -> One / Many Articles -> one author
-    pub_date = models.DateTimeField('date published')
-	article_id = models.ForeignKey(Article, on_delete=models.CASCADE)
+class Category(models.Model):
+    name = models.CharField(max_length=255)
 
     def __str__(self):
         return self.pk
 
-class Like(model.Models):
-    isLike = models.NullBooleanField()
-	author_id = models.ForeignKey(User) # User ID - Many -> One / Many Articles -> one author
+class Article(models.Model):
+    title = models.CharField(max_length=255)
+    text = models.TextField()
     pub_date = models.DateTimeField('date published')
-	article_id = models.ForeignKey(Article, on_delete=models.CASCADE)
+    author_id = models.ForeignKey(User, on_delete=models.CASCADE) # User ID - Many -> One / Many Articles -> One author
+    category_id = models.ForeignKey(Category, on_delete=models.CASCADE) # Category ID - One -> One / One Articles -> One Category
+
+    def __str__(self):
+        return self.pk
+
+class Comment(models.Model):
+    text = models.TextField()
+    author_id = models.ForeignKey(User) # User ID - Many -> One / Many Articles -> one author
+    pub_date = models.DateTimeField('date published')
+    article_id = models.ForeignKey(Article, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.pk
+
+class Like(models.Model):
+    isLike = models.NullBooleanField()
+    author_id = models.ForeignKey(User) # User ID - Many -> One / Many Articles -> one author
+    pub_date = models.DateTimeField('date published')
+    article_id = models.ForeignKey(Article, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.pk
