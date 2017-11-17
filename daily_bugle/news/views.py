f138d3d428aa393ec9e11b4b8b98a0bdfac43e26
nabilfadjar <nabilfadjar@gmail.com>
2017-11-17 16:08:35 +0000

    Added new views.py for Article

--- daily_bugle/news/views.py
+++ daily_bugle/news/views.py
@@ -1,3 +1,76 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Article, Category
+from django.http import JsonResponse
+
+#
+# Article Views
+#
+
+# views.request
+# Returns back one article object
+def article(request, article_id):
+    if request.method == 'GET': # Only accept GET request
+        searchedArticle = Product.objects.filter(pk=article_id)
+        jsonResponse_searchedArticle = dict()
+
+        # -- Article Model Format --
+        # title = models.CharField(max_length=255)
+        # text = models.TextField()
+        # pub_date = models.DateTimeField('date published')
+        # author_id = models.ForeignKey(User, on_delete=models.CASCADE) # User ID - Many -> One / Many Articles -> One author
+        # category_id = models.ForeignKey(Category, on_delete=models.CASCADE) # Category ID - One -> One / One Articles -> One Category
+
+        for article in searchedArticle:
+            jsonResponse_searchedArticle = {
+            "pk": article.pk,
+            "title": article.name,
+            "text": article.text,
+            "pub_date": article.description,
+            "author_id": article.price,
+            "category_id": article.price
+            }
+        return JsonResponse(jsonResponse_searchedArticle)
+    else:
+        print("views.request: Invalid request type made! Not a GET request.")
+        return HttpResponseBadRequest
+
+# views.article_category
+# Returns back all articles within the given category
+def article_category(request, category_name):
+    if request.method == 'GET': # Only accept GET request
+        try:
+            found_category_id = Category.objects.get(name=category_name)
+        except Category.DoesNotExist:
+            # If category_name is not found
+            print("views.request: Invalid request made! The category being searched does not exist.")
+            return HttpResponseBadRequest
+
+        searchedArticlesList = Article.objects.filter(category_id=found_category_id)
+        jsonResponse_articleList = dict()
+
+        # If no Articles found within the category
+        if not searchedArticlesList:
+            jsonResponse_searchedArticlesList = { "articles_found_count": 0 }
+            return JsonResponse(jsonResponse_searchedArticlesList)
+
+        # -- Article Model Format --
+        # title = models.CharField(max_length=255)
+        # text = models.TextField()
+        # pub_date = models.DateTimeField('date published')
+        # author_id = models.ForeignKey(User, on_delete=models.CASCADE) # User ID - Many -> One / Many Articles -> One author
+        # category_id = models.ForeignKey(Category, on_delete=models.CASCADE) # Category ID - One -> One / One Articles -> One Category
+
+        for article in searchedArticle:
+            jsonResponse_articleList.update(dict({
+            "pk": article.pk,
+            "title": article.name,
+            "text": article.text,
+            "pub_date": article.description,
+            "author_id": article.price,
+            "category_id": article.price
+            }))
+        return JsonResponse(jsonResponse_searchedArticle)
+    else:
+        print("views.request: Not invalid request made! Not a GET request.")
+        return HttpResponseBadRequest
