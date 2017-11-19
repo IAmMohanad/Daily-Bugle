6e05788f6d8b4a5d1ce7305c6ffc5421ba5d6632
Muhammad Nabil Fadhiya <nabilfadjar@gmail.com>
2017-11-19 17:37:36 +0000

    Created new View for article_add_new

--- daily_bugle/news/views.py
+++ daily_bugle/news/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, get_list_or_404
 
 from .models import Article, Category
 from django.http import JsonResponse
+
+from datetime import datetime
 
 #
 # Article Views
@@ -35,6 +37,38 @@ def article(request, article_id):
         print("views.request: Invalid request type made! Not a GET request.")
         return HttpResponseBadRequest
 
+# views.request
+# Returns back one article object
+def article_add_new(request):
+    if request.method == 'POST': # Only accept POST request
+        # -- Article Model Format --
+        # title = models.CharField(max_length=255)
+        # text = models.TextField()
+        # pub_date = models.DateTimeField('date published')
+        # author_id = models.ForeignKey(User, on_delete=models.CASCADE) # User ID - Many -> One / Many Articles -> One author
+        # category_id = models.ForeignKey(Category, on_delete=models.CASCADE) # Category ID - One -> One / One Articles -> One Category
+
+        new_article = request.POST
+        context = dict()
+
+        title = request.POST["new_title"]
+        text = request.POST["new_text"]
+        pub_date = datetime.now()
+        # author = request.session["logged_in_user"]
+        request.POST["category_id"]
+
+
+        try:
+            new_article = Article(title=title, text=text, pub_date=pub_date, author=test_user, category=test_category)
+            new_article.save()
+        except:
+            return JsonResponse({"isNewArticleAdded" : False})
+
+        return JsonResponse({"isNewArticleAdded" : True})
+    else:
+        print("views.request: Invalid request type made! Not a GET request.")
+        return HttpResponseBadRequest
+
 # views.article_category
 # Returns back all articles within the given category
 def article_category(request, category_name):
@@ -52,14 +86,14 @@ def article_category(request, category_name):
         context = dict()
 
         for article in articles:
-            context.update(dict({
+            context[article.pk] = {
                 "pk": article.pk,
                 "title": article.title,
                 "text": article.text,
                 "pub_date": article.pub_date,
-                "author_name": "Jack", # article.author_id,
-                "category_name": "Racing" # article.category_id
-            }))
+                "author_name": article.author.full_name,
+                "category_name": article.category.name
+            }
         return JsonResponse(context)
     else:
         print("views.request: Not invalid request made! Not a GET request.")
