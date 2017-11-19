aac05caf7c0a86af08588d5bf3cb1af6a56d8019
Muhammad Nabil Fadhiya <nabilfadjar@gmail.com>
2017-11-19 15:34:11 +0000

    Created first testcase

--- daily_bugle/news/tests.py
+++ daily_bugle/news/tests.py
@@ -1,13 +1,37 @@
 from django.test import TestCase
 
+# Imports all Models for Testing
+from .models import Article, Category, User
+from datetime import datetime
 
 #
 # Nabil's Test Cases
 #
 
-# Imports for Class ArticleModelTests
-from .models import Article
+class ArticleModelTests(TestCase):
 
-class ArticleModelTests(TestCase)
+    # Test creating an article model
     def test_create_article_model(self):
-        # Create new Article Model
+        test_user = User(full_name="Muhammad Nabil Fadhiya", email="nabil@me.com", password="NowYouSeeMe", phone_number="07478344545")
+        test_user.save()
+        test_category = Category(name="Racing")
+        test_category.save()
+
+        title = "Bacon Ipsum"
+        text = """Bacon ipsum dolor amet sausage pig venison, pork loin cow andouille shoulder
+        kevin pancetta meatloaf pastrami hamburger kielbasa salami biltong. Shank capicola tongue,
+        bresaola pork chop sausage kevin short loin. Alcatra buffalo drumstick beef ribs ball tip boudin t-bone
+        brisket meatball. Fatback swine salami, meatloaf sausage ball tip chicken porchetta pastrami bresaola
+        biltong prosciutto boudin doner flank. Turducken cow t-bone andouille.
+
+        Drumstick andouille buffalo swine, jowl pastrami cow chuck beef ribs. Doner short ribs andouille bacon.
+        Alcatra ham pastrami, jowl fatback doner buffalo t-bone. Swine turducken pastrami ham. Shankle ribeye bacon,
+        bresaola ball tip pork short loin frankfurter corned beef meatball. Turkey shoulder shankle, alcatra
+        tri-tip brisket turducken chuck sausage kielbasa. Flank shankle leberkas alcatra shoulder pork."""
+        pub_date = datetime.now()
+        author_id = 1
+        category_id = 1
+
+        test_article = Article(title=title, text=text, pub_date=pub_date, author_id=test_user, category_id=test_category)
+        test_article.save()
+        # print(test_article.title, test_article.text, test_article.pub_date, test_article.author_id, test_article.category_id)
