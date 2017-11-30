d15c2d733851681f6965717dd199e90216039fb4
mkowais <m_o_k@hotmail.co.uk>
2017-11-30 18:34:56 +0000

    Uncommented stuff needed

--- daily_bugle/news/urls.py
+++ daily_bugle/news/urls.py
@@ -6,22 +6,20 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
 
     # Sign Up Page
-    #url(r'^/signup$', views.signup, name='signup'),
+    url(r'^/signup$', views.signup, name='signup'),
     # Login Page
-    #url(r'^/login$', views.login, name='login'),
+    url(r'^/login$', views.login, name='login'),
     # User Page
-    #url(r'^/user/(?P<user_id>[0-9]+)$', views.user, name='user'),
+    url(r'^/user/(?P<user_id>[0-9]+)$', views.user, name='user'),
 
     # Article Page
-#    url(r'^/article/(?P<article_id>[0-9]+)$', views.article, name='article'),
-    ## Article filter by Category (Return list of Article IDs)
-#    url(r'^/ajax/article/category/(?P<category_name>[a-zA-Z]+$', views.article_category, name='article_category'),
+    url(r'^/article/(?P<article_id>[0-9]+)$', views.article, name='article'),
+    # Article filter by Category (Return list of Article IDs)
+    url(r'^/ajax/article/category/(?P<category_name>[a-zA-Z]+$', views.article_category, name='article_category'),
 
     # GET Like Object <- Fixed Regex
     url(r'^ajax/article/(?P<article_id>[0-9]+)/likes$', views.AllLikes, name='get_ALLlikes'), # GET - All Likes, POST - New Likes
     url(r'^ajax/article/(?P<article_id>[0-9]+)/addorDislike/(?P<isLike>[0-9]+)$', views.addorDislike, name='get_likesAndDislikes'), # GET - All Likes, POST - New Likes
-    # UPDATE (POST|DELETE) Like Object
-#    url(r'^/ajax/article/(?P<article_id>[0-9]+)/likes/(?P<isLike>^-1|[0|1])$', views.update_likes, name='update_likes'), # POST - New Comment, DELETE -  Comment (Put Comment ID in Body)
 
     # GET Comment Object
     url(r'^ajax/article/(?P<article_id>[0-9]+)/comments$', views.comment, name='comment'), # GET - All Comments, POST - New Comment
